e87a0d406592864174178b49db12f9a1aba2e2a9
dguillo87 <dguillo@cloudflare.com>
2025-06-07 17:14:27 +0200

    Ajout nouvelle reousrce

--- main.tf
+++ main.tf
@@ -35,20 +35,54 @@ resource "cloudflare_dns_record" "test_dash" {
   ttl     = 3600
 }
 
-resource "cloudflare_firewall_rule" "wordpress" {
+resource "cloudflare_dns_record" "test_git" {
   zone_id = "a3aa4938a1d46a31ecb34afc6a32af81"
-  action = {
-    mode = "block"
-    response = {
-      body = "<error>This request has been blocked.</error>"
-      content_type = "text/xml"
+  name    = "test-git"
+  content = "192.0.2.1"
+  type    = "A"
+  ttl     = 3600
+}
+
+resource "cloudflare_ruleset" "example_ruleset" {
+  kind = "root"
+  name = "My ruleset"
+  phase = "http_request_firewall_custom"
+  zone_id = "a3aa4938a1d46a31ecb34afc6a32af81"
+  description = "My ruleset to execute managed rulesets"
+  rules = [{
+    action = "block"
+    action_parameters = {
+      response = {
+        content = <<EOT
+        {
+          "success": false,
+          "error": "you have been blocked"
+        }
+        EOT
+        content_type = "application/json"
+        status_code = 400
+      }
     }
-    timeout = 86400
-  }
-  filter = {
-    description = "Wordpress break-in attempts that are outside of the office"
-    expression = "(http.request.uri.path ~ \".*wp-login.php\" or http.request.uri.path ~ \".*xmlrpc.php\") and ip.src ne 192.0.2.1"
-    paused = false
-    ref = "FIL-100"
-  }
+    description = "Block when the IP address is not 1.1.1.1"
+    enabled = true
+    exposed_credential_check = {
+      password_expression = "url_decode(http.request.body.form[\\\"password\\\"][0])"
+      username_expression = "url_decode(http.request.body.form[\\\"username\\\"][0])"
+    }
+    expression = "ip.src ne 1.1.1.1"
+    logging = {
+      enabled = true
+    }
+    ratelimit = {
+      characteristics = ["ip.src"]
+      period = 60
+      counting_expression = "http.request.body.raw eq \"abcd\""
+      mitigation_timeout = 600
+      requests_per_period = 1000
+      requests_to_origin = true
+      score_per_period = 400
+      score_response_header_name = "my-score"
+    }
+    ref = "my_ref"
+  }]
 }
