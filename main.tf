313ad55842da347a034ad9d22552202fab4b9c2e
dguillo87 <dguillo@cloudflare.com>
2025-06-08 20:05:57 +0200

    Mise à jour des records et rules avec le provider v4

--- main.tf
+++ main.tf
@@ -34,6 +34,14 @@ resource "cloudflare_record" "mon_dns2" {
   ttl     = 3600
 }
 
+resource "cloudflare_record" "test_dash" {
+  zone_id = "a3aa4938a1d46a31ecb34afc6a32af81"
+  name    = "test-dash"
+  content = "192.0.2.1"
+  type    = "A"
+  ttl     = 3600
+}
+
 resource "cloudflare_record" "test_git" {
   zone_id = "a3aa4938a1d46a31ecb34afc6a32af81"
   name    = "test-git"
@@ -42,6 +50,16 @@ resource "cloudflare_record" "test_git" {
   ttl     = 3600
 }
 
+resource "cloudflare_filter" "wordpress" {
+  zone_id  = "a3aa4938a1d46a31ecb34afc6a32af81"
+  expression = "(http.request.uri.path contains \"/wp-login.php\")"
+  description = "Block wp-login.php"
+}
 
-
+resource "cloudflare_firewall_rule" "block_wp_login" {
+  zone_id     = "a3aa4938a1d46a31ecb34afc6a32af81"
+  filter_id   = cloudflare_filter.wordpress.id
+  action      = "block"
+  description = "Block access to wp-login.php"
+}
 
